2921badf0f81364523b17a90677cb649c0d27287
parbzip <121424292+parbzip@users.noreply.github.com>
2024-11-23 02:41:52 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,10 +100,9 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-//pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-//pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-pub const RENDEZVOUS_SERVERS: &[&str] = &["vpn.xbmax.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "VerkvEcCSkzmGqTG0+h0j1kkw2z+y8KbsqR/40+LvY4=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
